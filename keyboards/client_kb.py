f996e12a4263866e48dc46e3e5452e0beb45c6c1
nefizik <timur24032002@gmail.com>
2022-04-02 15:51:04 +0300

    Added second keyboard

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -10,3 +10,9 @@ kb5 = KeyboardButton('Контакты')
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)  # замещает клавиатуру
 kb_client.add(kb1).add(kb2).insert(kb3).add(kb4).insert(kb5)
 
+
+lkkb1 = KeyboardButton(text='Регистрация')
+lkkb2 = KeyboardButton(text='Выход')
+
+lkkb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+lkkb_client.add(lkkb1).add(lkkb2)
