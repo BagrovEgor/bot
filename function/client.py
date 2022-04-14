472b58005045a9dd87748f368ada1116f4161344
nefizik <timur24032002@gmail.com>
2022-04-14 16:08:18 +0300

    Added database control elements

--- function/client.py
+++ function/client.py
@@ -7,6 +7,9 @@ from keyboards.client_kb import sskb_client
 from keyboards.client_kb import zvkb_client
 
 from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+
 
 import sqlite3 as sq
 
@@ -14,8 +17,7 @@ keyboard = 1
 
 def sql_start():
     global base, cur
-    base = sq.connect('basa.db')
-    cur = base.cursor()
+
 
 
 
@@ -26,14 +28,46 @@ async def greeting(message: types.Message):
 Чтобы начать, нажми кнопку или отправь сообщение!', reply_markup=kb_client)
 
 
+class Form(StatesGroup):
+    name = State()  # Will be represented in storage as 'Form:name'
+
+
+@dp.message_handler(state=Form.name)
+async def process_name(message: types.Message, state: FSMContext):
+
+    async with state.proxy() as data:
+        data['name'] = message.text
+
+    await Form.next()
+    await message.reply(f'Вы выбрали общежитие {message.text}')
+    user = message.from_user.id
+    print(user, message.text)
+
+    # добавление нового или обновление старого
+    base = sq.connect('basa.db')
+    cur = base.cursor()
+    dorm_id = cur.execute(f'select id from main.dorms where dorm == {message.text};').fetchone()
+    dorm_id = dorm_id[0]
+
+    isreg = cur.execute(f'select dorm from dorms_of_users where user_id == {user};').fetchone()
+
+    if (isreg == None):
+        cur.execute(f'INSERT INTO main.dorms_of_users(user_id, dorm) VALUES ({user}, {dorm_id});')
+    else:
+        cur.execute(f'UPDATE dorms_of_users SET dorm = {dorm_id} WHERE user_id == {user}')
+
+    base.commit()
+    base.close()
+
 async def lk(message: types.Message):
     await bot.send_message(message.from_user.id, 'ЛК', reply_markup=lkkb_client)
     keyboard = 1
 
-
 async def ob(message: types.Message):
     await bot.send_message(message.from_user.id, 'Общага', reply_markup=obkb_client)
     keyboard = 2
+    await Form.name.set()
+
 
 async def ss(message: types.Message):
     await bot.send_message(message.from_user.id, 'Студсовет', reply_markup=sskb_client)
