56c4f234d43d590ee06402aae62c1a8e140f3f8d
dddflow <d30082002@gmail.com>
2022-05-14 01:37:10 +0300

    Бот переехал -> @PolyDormsBot

--- dop.py
+++ dop.py
@@ -3,5 +3,5 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
-bot = Bot(token='5233751354:AAFsyIPagpgdW7K2z6W_n6V2BviXoesPKsk')
+bot = Bot(token='5241149232:AAEu-7_SdfpJCYuc7935X-6omy0QoE_NBNA')
 dp = Dispatcher(bot, storage=storage)  # объект для хэндлеров
